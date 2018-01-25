4cbfae96f1258b7fc38f4c16f90ce2911f2e0a39
Anil Kumar Nayak <anil.nayak@jellyglass.com>
2018-01-25 17:24:54 +0530

    chnages

---
+++ config.js
@@ -0,0 +1,8 @@
+export const config = {
+    slack:{
+        clientId:"304166466934.304347581908",
+        clientSecret:"0d81a3d1e4e3806ae8a82b8390031cb1",
+        verificationToken:"i4BBCEEGzoobjgUTxuXWhaSO",
+        url:"https://slack.com/oauth/authorize?client_id=304166466934.304347581908&scope=incoming-webhook"
+    }
+}
